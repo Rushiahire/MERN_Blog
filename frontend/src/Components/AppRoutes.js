36bfe0b84ab369b8a14d5c9f3ab0f3e01e99463b
rushikesh24 <rushikesh@8848digital.com>
2023-01-01 14:56:56 +0530

    routes n navbar

--- frontend/src/Components/AppRoutes.js
+++ frontend/src/Components/AppRoutes.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom";
+import BlogDetail from './BlogDetail';
+import Blogs from './Blogs';
 import Home from './Home';
-import Login from './Login';
+import AddBlog from './AddBlog';
+import UserBlogs from './UserBlogs';
+import Auth from './Auth';
 
 const AppRoutes = () => {
   return (
     <div>
         <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/login" element={<Login/>} />
+            <Route path="/auth" element={<Auth/>} />
+            <Route path="/blogs" element={<Blogs/>} />
+            <Route path="/my-blogs" element={<UserBlogs/>} />
+            <Route path="/my-blogs/:id" element={<BlogDetail/>} />
+            <Route path="/blogs/add-blogs" element={<AddBlog/>} />
+
         </Routes>
     </div>
   )
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div>
-        <h2>
-            navbar page
-        </h2>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+        <div class="container-fluid">
+          <Link className='navbar-brand fs-2 mx-5 text-danger ' to="/"><b>BLOGS</b></Link>
+          {/* <a class="navbar-brand" href="/">BLOGS</a> */}
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link to="/blogs" className='nav-link active'>
+                  ALL BLOGS
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link to="/my-blogs/" className='nav-link'>
+                  MY BLOGS
+                </Link>
+              </li>
+
+            </ul>
+            <form class="d-flex" role="search">
+              <Link to="/auth">
+                <button className='btn btn-primary text-uppercase mx-1 rounded-4'>
+                  login
+                </button>
+              </Link>
+              <Link to="/auth">
+                <button className='btn btn-primary text-uppercase mx-2 rounded-4'>Signup</button>
+              </Link>
+              <Link to="/auth">
+                <button className='btn btn-primary text-uppercase rounded-4'>logout</button>
+              </Link>
+
+            </form>
+          </div>
+        </div>
+      </nav>
+
+
     </div>
   )
 }
